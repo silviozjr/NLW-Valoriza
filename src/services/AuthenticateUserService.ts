3d32d01a990edd5d865f6fafbf74d740ad9b3068
silviozjr <silviozjr@gmail.com>
2021-06-24 20:54:02 -0300

    Aula 4 - JWT

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,50 @@
+import { getCustomRepository } from "typeorm";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories);
+
+    // Verificar se email existe
+    const user = await usersRepositories.findOne({
+      email,
+    });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    // verificar se senha está correta
+
+    // 123456 / 783645734-sdhfhsdf7762374234234
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect");
+    }
+
+    // Gerar token
+    const token = sign(
+      {
+        email: user.email,
+      },
+      "641d47ee6b6810eddf4dd3c98c8f086e",
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService };
